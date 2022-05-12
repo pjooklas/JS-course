b2541d8c01f271529e8df8cb55851939ff38ab84
pjooklas <pjooklas@users.noreply.github.com>
2022-05-12 21:24:32 +0300

    konversija

---
+++ konversija.js
@@ -0,0 +1,27 @@
+// let a = 1;
+// let b = 2;
+// let c = "3";
+// let d = a + b + c;
+// //      1 + 2
+// //        3   + "3"
+// //       "3"  + "3"
+// //          "33"
+
+// let e = c + b + a;
+// //      "3" + 2
+// //      "3" + "2"
+// //        "32" + 1
+// //        "32" + "1"
+// //          "321"
+
+
+// console.log(a, b, c, d, e);
+
+
+let b = "0";
+
+if (b == false) {
+  console.log("true");
+} else {
+  console.log("false");
+}
