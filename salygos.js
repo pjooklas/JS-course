95d37a03747e558f65f3d7fa97af7a35466f87dd
pjooklas <pjooklas@users.noreply.github.com>
2022-05-12 18:21:20 +0300

    JS basics

---
+++ salygos.js
@@ -0,0 +1,15 @@
+let sk = -2;
+
+// if (sk < 0) {
+//     console.log('neigiamas');
+// } else if (sk < 10) {
+//     console.log('mazas');
+// } else if (sk < 100) {
+//     console.log('vidutinis');
+// } else
+//     console.log('didelis');
+
+if (sk > 100) console.log('didelis');
+else if (sk >= 10) console.log('vidutinis');
+else if (sk >= 0) console.log('mazas');
+else console.log('neigiamas');
